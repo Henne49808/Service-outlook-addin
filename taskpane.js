1a1da13588884dbbed012fef8aad77aea026438e
Henne49808 <frank.hendrischk@hedelius.de>
2026-07-06 10:50:08 +0200

    Update ADDIN_BUILD version to 20260701.15

--- taskpane.js
+++ taskpane.js
@@ -31,7 +31,7 @@ incidentClosedStatus: 281370004,
 sapForwardTargetStatusValue: 281370007,
 };
 const ADDIN_VERSION = "1.0.4";
-const ADDIN_BUILD   = "20260701.14"20260701.14"20260701.14";
+const ADDIN_BUILD   = "20260701.15";
 const EMPTY_CUSTOMERS = ["NONAME"];
 let currentState = {
     incidentId: null,
